85bf7d0c94941f384115a55e2b21520e21a61849
Terence Martin <github@nurdz.com>
2021-12-05 21:22:38 -0800

    Add in some explanatory docs on OAUTHf flow
    While things are still somewhat clear in the ol' noggin, add in some
    notes on the two OAUTH flows that we're using so that when it comes
    time to fully implement and document things we have some small hopes of
    remembering.

--- dashboard/script.js
+++ dashboard/script.js
@@ -15,25 +15,68 @@ refreshAuthURL();
 
 // OAuth Client Credentials Flow - App Token
 //------------------------------------------
+//
+// An Application token grants your application the right to make requests from
+// the Twitch back end servers. Such requests are not associated with any
+// particular user, and as such no user needs to authorize anything in order to
+// obtain one.
+//
+// That said, some requests may ask for data specific to a user; in order for
+// that data to be provided back the user must at some point have granted access
+// to the information using a different authorization flow. The association here
+// is drawn between the user at some point having authorized an application with
+// a particular client_id to obtain the requested information.
+//
+// As such, one need only make a POST request to the required endpoint in order
+// to obtain a token:
+//
+// POST https://id.twitch.tv/oauth2/token
+//    ?client_id=<your client ID>
+//    &client_secret=<your client secret>
+//    &grant_type=client_credentials
+//    &scope=<space-separated list of scopes>
 
 // OAuth Authorization Code Flow - User Access Token
 //--------------------------------------------------
-/*
-
-1. Send the user to our redirect URL; it will take you to auth
-
-https://id.twitch.tv/oauth2/authorize?client_id=q0xqvkmbfl8834g1rvqamjwbvxf3if&redirect_uri=http%3A%2F%2Flocalhost%3A9090%2Fauth%2Ftwitch%2Fcallback&response_type=code&scope=user:read:email
-
-
-GET https://id.twitch.tv/oauth2/authorize
-    ?client_id=<your client ID>
-    &redirect_uri=<your registered redirect URI>
-    &response_type=code
-    &scope=<space-separated list of scopes>
-    &state=BLERBYBLURB
-
-After the user authenticates, you get redirected to the given redirect URL,
-which should be a back end URL that allows the server to make a request to get
-a token based on the code provided in the callback URL.
-*
-*/
+//
+// A User Access Token grants your application the right to take actions as
+// another user. In this case that means allowing the bot to take actions as
+// either you or your dedicated bot account. To get such a token you need to
+// request that the user authorize their account, specifying the list of
+// permissions (known as scopes) that you want access to.
+//
+// Send the user to our redirect URL; it will take you to auth
+//
+//   GET https://id.twitch.tv/oauth2/authorize
+//       ?client_id=<your client ID>
+//       &redirect_uri=<your registered redirect URI>
+//       &response_type=code
+//       &scope=<space-separated list of scopes>
+//       &state=randombutsecureddatstring
+//
+// After the user authenticates, you get redirected to the given redirect URL,
+// which should be a back end URL that allows the server to make a request to get
+// a token based on the code provided in the callback URL.
+//
+// Regardless of wether the user authorizes or not, the request will come back to
+// our registered redirect URI; the response that comes back will have a code in it
+// if they said yes, and no code if no.
+//
+// The code needs to be handed to the back end in order to fetch the appropriate
+// token by making a POST request to https://id.twitch.tv/oauth2/token:
+//
+//   const response = await axios({
+//     url: 'https://id.twitch.tv/oauth2/token',
+//     method: 'POST',
+//     data: {
+//       client_id: TWITCH_CLIENT_ID,
+//       client_secret: TWITCH_SECRET,
+//       code,
+//       grant_type: 'authorization_code',
+//       redirect_uri: CALLBACK_URL
+//     }
+//   });
+//
+// As a security measure, if the original request includes a state paramter, the
+// value of that will be returned with the redirect so that we can be sure that the
+// response that comes back is genuine.
