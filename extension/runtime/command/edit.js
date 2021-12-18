fbc6bda2d4d1a9a324ccce8bbf4704c584648719
Terence Martin <github@nurdz.com>
2021-12-17 21:21:17 -0800

    Include the enable and disable commands
    This implements the command and the alias on the command that allow the
    user to control whether or not commands are enabled without having to
    modify the database directly.
    
    This currently refuses to modify the state of a core command to avoid
    issues like disabling the command that enables commands, locking you out
    of commands.

--- extension/runtime/command/edit.js
+++ extension/runtime/command/edit.js
@@ -4,16 +4,6 @@
 // =============================================================================
 
 
-/* When enabling or disabling commands, these specify the values the parameters
- * can take and what they map to. */
-const enable_options = {
-  true: 1,
-  on: 1,
-
-  false: 0,
-  off: 0
-};
-
 /* When changing the access level of a command, this specifies what values the
  * argument can take and what the resulting edited value should be. */
 const access_options = {
@@ -173,73 +163,35 @@ function get_command_info(api, details, userInfo) {
 // =============================================================================
 
 
-/* This command provides support for changing the state of commands on the fly,
- * allowing you to adjust their access levels, cooldowns and whether or not
- * they're enabled.
+/* This command allows you to enable or disable a command in order to control if
+ * it is accessible or not. Unlike the access level and cooldown checks, a
+ * command that is disabled cannot be executed by anyone.
  *
- * Currently this requires you to specify a single command; in future this could
- * be adjusted to allow for an arbitrarily long list of commands. */
-function edit_command(api, details, userInfo) {
-  // There should be exactly 1 word in the word list, which will tell us the
-  // command that we're working with. If that's not the case, display usage
-  // information and exit.
-  if (details.words.length !== 1) {
-    api.chat.say(`Usage: ${details.command} enable=true|false ` +
-                 `access=level cooldown=secs <command> ; ` +
-                 `change any options, or just give a command to see the current state`);
+ * This function serves both the enable and disable option and can determine
+ * which of the two it's supposed to do based on the name that it's invoked
+ * with. */
+async function change_enabled_state(api, details, userInfo) {
+  // We need to be given a command name.
+  if (details.words.length === 0) {
+    api.chat.say(`Usage: ${details.command} command`);
     return;
   }
 
-  // Look up the command to be viewed or edited; we can leave if it does not
-  // exist.
-  const cmd = api.commands.find(details.words[0]);
+  // Get the target command or leave; on error, this displays an error for us.
+  const cmd = getCommand(api, details, details.words[0], false, true);
   if (cmd === null) {
-    api.chat.say(`there is no command named ${details.words[0]} ; check spelling?`)
     return;
   }
 
-  // Based on the values of different parameters, add in fields that we can use
-  // to adjust the state of the command.
-  const mods = { };
-  const enable = details.params.get('enable');
-  const access = details.params.get('access');
-  const cooldown = details.params.get('cooldown');
+  // The command can either enable or disable a command; how this works depends
+  // on the name that it's invoked with.
+  const enabled = (details.command.endsWith('enable'));
 
-  // Is the user trying to change the enable/disable state?
-  if (enable !== undefined) {
-    mods.enabled = enable_options[enable];
-    if (mods.enabled === undefined) {
-      api.chat.say(`Invalid value for enable: possible values are ${Object.keys(enable_options).join(', ')}`);
-      return;
-    }
-  }
-
-  // Is the user trying to change the access level?
-  if (access !== undefined) {
-    mods.userLevel = access_options[access];
-    if (mods.userLevel === undefined) {
-      api.chat.say(`Invalid value for access: possible values are ${Object.keys(access_options).join(', ')}`);
-      return
-    }
-  }
-
-  // Is the user trying to change the cooldown time?
-  if (cooldown !== undefined) {
-    mods.cooldown = parseInt(cooldown) * 1000;
-    if (isNaN(mods.cooldown) === true) {
-      api.chat.say(`Invalid value for cooldown; expected a time in seconds`);
-      return
-    }
-  }
-
-  api.log.info(mods);
-  if (Object.keys(mods).length === 0) {
-    api.chat.say(`command details: ${cmd.name}, ${cmd.enabled} ${cmd.userLevel} ${cmd.cooldown}`);
-    return;
-  }
-
-
+  // Update the command with the changes, then report them.
+  await storeCmdChanges(api, cmd, { enabled })
+  api.chat.say(`the ${cmd.name} command status has been set to ${enabled === true ? 'enabled' : 'disabled'}`);
 }
+
 
 // =============================================================================
 
@@ -253,12 +205,6 @@ function edit_command(api, details, userInfo) {
 //
 //
 // Commands that we want:
-//   $enable [command]
-//   $disable [command]
-//   - Enable or disable a particular command; make sure that it can't disable
-//     itself to avoid future comedic effect. Implement this as a single command
-//     that knows what to do based on the name it's invoked with.
-//
 //   $access [command] <level>
 //   - View or edit the access level of the commad; if no level is given this
 //     will display it instead. For security purposes this could not allow
@@ -277,6 +223,9 @@ function edit_command(api, details, userInfo) {
 //     command, or see the list of aliases for a command. In this one instance
 //     this can be invoked on an alias. It will display information on the
 //     aliases available for whatever the command is.
+//
+//     Aliases should not be allowed on core commands, since they may rely on
+//     the name they're invoked with to know what to do.
 
 
 
@@ -286,7 +235,7 @@ function edit_command(api, details, userInfo) {
 module.exports = {
   load: async api => {
     return {
-      '$enable': stub_command,
+      '$enable': change_enabled_state,
       '$access': stub_command,
       '$cooldown': stub_command,
       '$alias': stub_command,
