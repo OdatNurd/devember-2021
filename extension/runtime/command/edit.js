87679a0204a6990d90559a81e91763a1f2359008
Terence Martin <github@nurdz.com>
2021-12-17 21:21:17 -0800

    Include some helpers for upcoming changes.
    This includes three helper functions to be used by most/all of the
    commands in the editing code.
    
    The first is for getting a target command and verifying that it follows
    specific rules, such as existing, being or not being an alias or whether
    it is a core command or not.
    
    The second is for storing changes made to a command record, which will
    update the database and also patch the currently loaded command record.
    
    The third is a visual helper for being able to show cooldown times in a
    human readable way. This last one is not currently very good, but it's
    good enough for our purposes here.

--- extension/runtime/command/edit.js
+++ extension/runtime/command/edit.js
@@ -55,6 +55,78 @@ const access_display = [
 // =============================================================================
 
 
+/* Given a command name, find and return the entry in the command list that
+ * represents that command and return it back. The allowAlias argument indicates
+ * whether or not it's permissible for the command being looked up to be an alias
+ * of some other command.
+ *
+ * The return value is the command entry on success or null on failure, which
+ * includes violating constraints on what's allowed.
+ *
+ * When this returns null, it generates a message to the chat that indicates
+ * that the command being executed cannot be executed. */
+function getCommand(api, details, name, allowAlias, allowCore) {
+  // Find the command named in the command list; if this is null we can
+  // immediately generate an error and return back.
+  const cmd = api.commands.find(name);
+  if (cmd === null) {
+    api.chat.say(`unable to locate command ${name}; did you spell it correctly and provide a command prefix?`);
+    return null;
+  }
+
+  // If this is an alias but they're not allowed, then reject the command.
+  if (allowAlias === false && cmd.name !== name) {
+    api.chat.say(`${details.command} does not modify aliased commands; did you mean to modify ${cmd.name}?`);
+    return null;
+  }
+
+  if (allowCore === false && cmd.core === true) {
+    api.chat.say(`${details.command} does not modify core commands; to avoid executing them, try the 'role' command`);
+    return null;
+  }
+
+  return cmd;
+}
+
+
+// =============================================================================
+
+
+/* This takes a dictionary that contains field changes that should be applied to
+ * the command provided and update the record in the database using them, based
+ * on the id of the command that is provided.
+ *
+ * This only touches the fields provided and leaves everything else alone. */
+async function storeCmdChanges(api, cmd, changes) {
+  // Get the database model for commands and perform the update.
+  const model = api.db.getModel('commands');
+  await model.update({ id: cmd.id }, changes);
+
+  // We also need to apply the changes directly to the command, or they won't
+  // be seen until it reloads and refreshes its information from the database.
+  Object.keys(changes).forEach(key => cmd[key] = changes[key]);
+}
+
+
+// =============================================================================
+
+
+/* Given a command cooldown time in milliseconds, return back a more human
+ * readable interpretation of it. */
+function displayableCooldown(cooldown) {
+  // On a level of 1 to 10, where 1 is gross and 10 is awesome, this rates a
+  // disgusting; but it's good enough for the time being.
+  return new Date(cooldown)
+    .toISOString()
+    .substr(11, 8)
+    .replace(':', 'h ')
+    .replace(':', 'm ') + 's';
+}
+
+
+// =============================================================================
+
+
 function stub_command(api, details, userInfo) {
   api.chat.say('I\'m a little teapot');
 }
