22a5260b3a2379d9daf7e15ad73efee1ff776105
Terence Martin <github@nurdz.com>
2021-12-25 22:45:18 -0800

    Fix the name of the responders schema file
    During the period where we were waffling on what to call the name of the
    schema for auto responders, we named the file responses and then
    decided that the name responders was better, but didn't fix the file.
    
    Since that is very nerve wracking and breaks the pattern, that is now
    fixed.

--- extension/db/schema/index.js
+++ extension/db/schema/index.js
@@ -6,7 +6,7 @@ const { TokenSchema } = require('./tokens');
 const { UserSchema } = require('./users');
 const { CommandSchema } = require('./commands');
 const { RegularUserSchema } = require('./regulars');
-const { ResponderSchema } = require('./responses');
+const { ResponderSchema } = require('./responders');
 
 
 // =============================================================================
