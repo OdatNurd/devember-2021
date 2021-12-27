6e747871f97b5735674c8d40988ef6d41e3f5b13
Terence Martin <github@nurdz.com>
2021-12-27 10:23:37 -0800

    Finalize the initial version of the text command
    This finishes the initial implementation of the text command's ability
    to add, remove and view link responders.
    
    There is still a bit of cleanup that could be done here (and though all
    core commands) cleaning up the error messages and responses given.

--- extension/runtime/command/text.js
+++ extension/runtime/command/text.js
@@ -1,8 +1,7 @@
-
 // =============================================================================
 
 
-const { usage, getValidCmdName, persistItemChanges,
+const { dedent: _, usage, getValidCmdName, persistItemChanges,
         cooldownToString, stringToCooldown,
         userLevelToString, stringToUserLevel, getDisplayAccessLevels } = require('../../utils');
 
@@ -139,13 +138,184 @@ async function edit_text_response(api, cmd, userInfo) {
 // =============================================================================
 
 
-// $text link $name $linkedname
-// $text unlink $name $linkedname
+/* This helper for adding in a link assumes that it's being called from a valid
+ * link add context to perform the work of validating input and then adding in
+ * a new link.
+ *
+ * This will check the remaining arguments for validity and, if all is well,
+ * return back an array of items that represents the responder that is going to
+ * have a link added and an update object to be used to update the database
+ * to put the new link in effect.
+ *
+ * On any error, this returns a null responder and an empty update after
+ * displaying an error message describing the situation. A null responder is
+ * also returned for cases where just a responder name is provided, since in
+ * that case we display the list of links instead of adding and nothing
+ * further needs to be done. */
+function handle_link_add(api, cmd, userInfo) {
+  const errReturn = [null, null, {}];
+
+  // To add a link, we need to have at least 2 arguments; the link operation
+  // and the responder to link. A third argument means we're creating a link
+  // while a lack of one means just to display the targets of the link instead.
+  if (cmd.words.length < 2) {
+    usage(api, cmd, 'link <responder> [linkname]', `add a link for the given
+      responder trigger it via another name; specify no link name to view links
+      for the given responder`);
+    return errReturn;
+  }
+
+  // Gather all arguments.
+  const nameArg = getValidCmdName(api, cmd.words[1]);
+  const linkArg = getValidCmdName(api, cmd.words[2]);
+
+  // This is either an add or a display, but either way we need to find the
+  // responder that is being targeted here.
+  const target = api.responders.find(nameArg);
+  if (target === null) {
+    api.chat.say(`a responder named ${nameArg} could not be found`);
+    return errReturn;
+  }
+
+  // If we were not given a target link, we're being asked to display the links
+  // for a responder instead, so do that now and leave.
+  if (linkArg === undefined) {
+    api.chat.say(_(`links for the ${target.name} responder :
+                   ${(target.aliases.length === 0) ? 'none': target.aliases.join(',')}`));
+
+    // Not really an error, but not really an add either; the caller knows that
+    // a null return means to do nothing further.
+    return errReturn;
+  }
+
+  // We have a potential link; if we can find something by that name, then this
+  // can't be a link target or it would collide.
+  const checkLink = api.commands.find(linkArg);
+  if (checkLink !== null) {
+    api.chat.say(`${linkArg} cannot be used as a responder link; it already resolves to ${checkLink.name}`);
+    return errReturn;
+  }
+
+  // This appears to be a valid add operation.
+  return [target, linkArg, { aliases: [...target.aliases, linkArg] }];
+}
+
+
+// =============================================================================
+
+
+/* This helper for removing in a link assumes that it's being called from a
+ * valid link remove context to perform the work of validating input and then
+ * removing a responder link.
+ *
+ * This will check the remaining arguments for validity and, if all is well,
+ * return back an array of items that represents the responder that is going to
+ * have a link removed and an update object to be used to update the database
+ * to put the removal into effect.
+ *
+ * On any error, this returns a null responder and an empty update after
+ * displaying an error message describing the situation. */
+function handle_link_remove(api, cmd, userInfo) {
+  const errReturn = [null, null, {}];
+
+  // For an unklink, we only need two arguments; the unlink operation and the
+  // link that is supposed to be removed.
+  if (cmd.words.length < 2) {
+    usage(api, cmd, 'unlink <responder>', `remove the given responder link`);
+    return errReturn;
+  }
+
+  // Gather all arguments.
+  const nameArg = getValidCmdName(api, cmd.words[1]);
+
+  // Get the link that is supposed to be removed. If this doesn't exist we
+  // cannot continue.
+  const alias = api.responders.find(nameArg);
+  if (alias === null) {
+    api.chat.say(`a responder named ${nameArg} could not be found`);
+    return errReturn;
+  }
+
+  // In order to remove this item, it needs to be an actual link; if it's a
+  // main responder we can't do anything.
+  if (alias.name === nameArg) {
+    api.chat.say(`${alias.name} is not a link; it cannot be removed by this command`);
+    return errReturn;
+  }
+
+  return [alias, nameArg, { aliases: alias.aliases.filter(name => name !== nameArg)}];
+}
+
+
+// =============================================================================
+
+
+/* This handles creating and removing links to existing responders (these are
+ * known as aliases in commands and share the same name here due to sharing some
+ * underlying code). */
 async function modify_text_link(api, cmd, userInfo) {
+  const validOps = ['link', 'unlink'];
+
+  // At a minimum, we need to receive at least one argument, which will tell us
+  // what we're trying to do. That argument needs to in the valid list.
+  if (cmd.words.length < 1 || validOps.indexOf(cmd.words[0]) === -1) {
     return usage(api, cmd, '[link|unlink] <name> [link]', `create a new responder
       that links to an existing responder and shares its text, or remove an
-      existing link`);
+      existing link; specify only a responder to show links to that responder`);
+  }
 
+  // Gather all arguments.
+  const opName = cmd.words[0];
+
+  let responder = undefined;
+  let link = undefined;
+  let update = undefined;
+  let postUpdate = undefined;
+
+  switch(opName) {
+    case 'link':
+      console.log('do link');
+      // Handle the add using our helper. This does error checking and returns
+      // a null target link on error.
+      [responder, link, update] = handle_link_add(api, cmd, userInfo);
+
+      // If we got a link, set up a handler to put the change in effect.
+      if (responder !== null) {
+        postUpdate = () => api.responders.addAlias(responder.name, link);
+      }
+      break;
+
+    case 'unlink':
+      console.log('do unlink');
+      // Handle the removal using the helper. This does error checking and will
+      // return a target link on error.
+      [responder, link, update] = handle_link_remove(api, cmd, userInfo);
+
+      // If we got a link, set up a handler to put the change in effect.
+      if (responder !== null) {
+        postUpdate = () => api.responders.removeAlias(responder.name, link);
+      }
+      break;
+
+    default:
+      api.log.error(`while handling ${cmd.name}, got unhandled operation ${opName}`);
+      return;
+  }
+
+  // If we get to this point and responder is null, then the add or remove
+  // operation was not valid for some reason, so just leave.
+  if (responder === null) {
+    return;
+  }
+
+  // Update the item with the changes, then report them.
+  await persistItemChanges(api, 'responders', responder, update);
+  if (postUpdate !== undefined) {
+    postUpdate();
+  }
+
+  api.chat.say(_(`links for the ${responder.name} responder are now:
+                 ${(responder.aliases.length === 0) ? 'none': responder.aliases.join(',')}`));
 }
 
 
