e21f362c5ff60d598a67a91d16b256ea1bd058f3
Terence Martin <github@nurdz.com>
2021-12-27 00:22:06 -0800

    Implement the text responder command
    This implements the command (and all sub-commands thereof) of the text
    command that allows for dynamically modifying the list of auto response
    items in the database.
    
    This based on how the command system is modeled, although the items
    being changed work in a different way.

--- extension/runtime/command/text.js
+++ extension/runtime/command/text.js
@@ -2,37 +2,137 @@
 // =============================================================================
 
 
-const { usage } = require('../../utils');
+const { usage, getValidCmdName, persistItemChanges,
+        cooldownToString, stringToCooldown,
+        userLevelToString, stringToUserLevel, getDisplayAccessLevels } = require('../../utils');
+
 
 
 // =============================================================================
 
 
+/* This handles the addition of a new text responder by making sure that an
+ * unused response name is given, along with some text. It will make sure that
+ * the table is updated as appropriate and then make sure that the new entry is
+ * available for immediate use. */
+async function add_text_response(api, cmd, userInfo) {
+  // We know that this is an add operation but we need to be given the name of
+  // the new item and at least one word of text.
+  if (cmd.words.length < 3) {
+    return usage(api, cmd, 'add <name> <text>', `create a new response item with
+      the given name which, when triggered, will say the specific text`);
+  }
 
-// $text add $name the text goes here
-function add_text_response(api, cmd, userInfo) {
-  return usage(api, cmd, 'add <name> <text>', `create a new response item with
-    the given name which, when triggered, will say the specific text`);
+  // Collect our arguments
+  let [opArg, nameArg, ...textArg] = cmd.words;
+  nameArg = getValidCmdName(api, nameArg);
+
+  // If the name given is already in the list of responders, we can't add one
+  // because it would clash.
+  if (api.responders.find(nameArg) !== null) {
+    api.chat.say(`unable to add a new responder for ${nameArg}; an item by this name already exists`);
+    return;
+  }
+
+  // Get the text that the responder will respond with; this is all unused words
+  // in the original input.
+  const responseText = textArg.join(' ');
+  api.chat.say(`creating a new text response named ${nameArg} => '${responseText}'`);
+
+  // Add the new entry to the database and then get the handler list to add the
+  // stub, which will bring the item into the list for us.
+  await api.db.getModel('responders').create({
+    name: nameArg,
+    aliases: [],
+    text: responseText
+  })
+  await api.responders.addItemStub(nameArg);
 }
 
 
 // =============================================================================
 
 
-// $text remove $name
-function remove_text_response(api, cmd, userInfo) {
-  return usage(api, cmd, 'remove <name>', `remove the response item with the
-    given name`);
+/* This handles the removal of a text responder by making sure that the name
+ * given is a responder that exists and is not an alias. It will make sure that
+ * the entry is removed from the database and that the internal list is also
+ * updated. */
+async function remove_text_response(api, cmd, userInfo) {
+  // We know that this is a remove operation but we need to be given the name of
+  // the item to remove.
+  if (cmd.words.length < 2) {
+    return usage(api, cmd, 'remove <name>', `remove the response item with the
+      given name`);
+  }
+
+  // Collect our arguments
+  const nameArg = getValidCmdName(api, cmd.words[1]);
+
+  // Look up the entry for the item that we want to remove. This has to exist
+  // or we can't remove it.
+  const item = api.responders.find(nameArg);
+  if (nameArg === null) {
+    api.chat.say(`unable to remove responder ${nameArg} because it does not exist`);
+    return;
+  }
+
+  // If the item is an alias for something else, we can't remove it; there's a
+  // distinct command that handles that.
+  if (nameArg !== item.name) {
+    api.chat.say(`unable to remove responder ${nameArg} because it is an alias; did you mean ${item.name}?`);
+    return;
+  }
+
+  // We can remove the item now
+  api.chat.say(`removing responder ${nameArg}`);
+
+  // Remove the item from the database, and then remove all traces of it from
+  // the internal list as well.
+  await api.db.getModel('responders').remove({ id: item.id });
+  api.responders.remove(nameArg);
 }
 
 
 // =============================================================================
 
 
-// $text edit $name the next text goes here
-function edit_text_response(api, cmd, userInfo) {
-  return usage(api, cmd, 'edit <name> <text>', `alter the text of an existing
-    response item to the specific text`);
+/* This handles editing the text associated with a specifically named responder
+ * by updating the database and also the entry that has already been loaded from
+ * the database. This ensures that the item to be edited exists and is not an
+ * alias for some other responder. */
+async function edit_text_response(api, cmd, userInfo) {
+  // We know that this is an edit operation but we need to be given the name of
+  // the item to edit and at least one word of text to change the item to.
+  if (cmd.words.length < 3) {
+    return usage(api, cmd, 'edit <name> <text>', `alter the text of an existing
+      response item to the specific text`);
+  }
+
+  // Collect our arguments
+  let [opArg, nameArg, ...textArg] = cmd.words;
+  nameArg = getValidCmdName(api, nameArg);
+
+  // If the name given is not in the list of responders, we can't edit it.
+  const item = api.responders.find(nameArg);
+  if (item === null) {
+    api.chat.say(`unable to edit responder ${nameArg} because it does not exist`);
+    return;
+  }
+
+  // If the item is an alias for something else, we can't edit it
+  if (nameArg !== item.name) {
+    api.chat.say(`unable to edit responder ${nameArg} because it is an alias; did you mean ${item.name}?`);
+    return;
+  }
+
+  // Get the text that the responder will respond with; this is all unused words
+  // in the original input.
+  const responseText = textArg.join(' ');
+  api.chat.say(`editing the response for ${nameArg} => '${responseText}'`);
+
+  // Update the database entry for this, and hot-patch the new data directly
+  // into the loaded instance.
+  await persistItemChanges(api, 'responders', item, { text: responseText });
 }
 
 
@@ -41,7 +141,7 @@ function edit_text_response(api, cmd, userInfo) {
 
 // $text link $name $linkedname
 // $text unlink $name $linkedname
-function modify_text_link(api, cmd, userInfo) {
+async function modify_text_link(api, cmd, userInfo) {
     return usage(api, cmd, '[link|unlink] <name> [link]', `create a new responder
       that links to an existing responder and shares its text, or remove an
       existing link`);
@@ -52,21 +152,103 @@ function modify_text_link(api, cmd, userInfo) {
 // =============================================================================
 
 
-// $text access $name <level>
-function modify_text_access(api, cmd, userInfo) {
-    return usage(api, cmd, 'access <name> <level>', `alter the user access level
-      that is required to trigger the named autoresponder; with no level, view
-      the currently set access level`);
+/* This modifies the access level required in order to trigger the provided
+ * text responder. This operation requires that the responder exists, but allows
+ * you to modify the access for a responder through a link. */
+async function modify_text_access(api, cmd, userInfo) {
+  // The levels we use in our display here; the actual values supported is a
+  // much larger list, to make commands more natural.
+  const levels = getDisplayAccessLevels();
+
+  // We need to be given a responder name.
+  if (cmd.words.length < 1) {
+    return usage(api, cmd, 'access <name> [${levels.join('|')}]', `alter the user
+      access level that is required to trigger the named autoresponder; with no
+      level, view the currently set access level`);
+  }
+
+  // Gather all arguments.
+  const nameArg = getValidCmdName(api, cmd.words[1]);
+  const intervalArg = cmd.words[2];
+
+  // If the name given is not in the list of responders, we can't edit it.
+  const item = api.responders.find(nameArg);
+  if (item === null) {
+    api.chat.say(`unable to edit responder ${nameArg} because it does not exist`);
+    return;
+  }
+
+  // If there is only a responder name provided, then display the current access
+  // level for this item and we can leave.
+  if (cmd.words.length === 2) {
+    api.chat.say(`the access level for ${item.name} is currently set to ${userLevelToString(item.userLevel)}`);
+    return;
+  }
+
+  // Look up the appropriate user level based on the argument provided.
+  const userLevel = stringToUserLevel(levelArg);
+  if (userLevel === undefined) {
+    api.chat.say(`${levelArg} is not a valid access level; valid levels are: ${levels.join(',')}`);
+    return;
+  }
+
+  // Update the command with the changes, then report them.
+  await persistItemChanges(api, 'responders', item, { userLevel });
+  api.chat.say(`the access level for ${item.name} has been set to ${userLevelToString(userLevel)}`);
 }
 
 // =============================================================================
 
 
-// $text cooldown $name <period>
-function modify_text_cooldown(api, cmd, userInfo) {
+/* This modifies the access level required in order to trigger the provided
+ * text responder. This operation requires that the responder exists, but allows
+ * you to modify the access for a responder through a link. */
+async function modify_text_cooldown(api, cmd, userInfo) {
+  // The possible formats for times that we use in our display here; the actual
+  // values are parsed from user input in this style.
+  const specs = ['##.#s', '##.#m', '##.#h'];
+
+  // We need to be given a responder name.
+  if (cmd.words.length < 1) {
     return usage(api, cmd, 'cooldown <name> <period>', `alter the cooldown
       period between invocations of the named respoonder; with no period, view
       the currently set cooldown period`);
+  }
+
+  // Gather all arguments.
+  const nameArg = getValidCmdName(api, cmd.words[1]);
+  const intervalArg = cmd.words[2];
+
+  // If the name given is not in the list of responders, we can't edit it.
+  const item = api.responders.find(nameArg);
+  if (item === null) {
+    api.chat.say(`unable to edit responder ${nameArg} because it does not exist`);
+    return;
+  }
+
+  // If there is only a responder name provided, then display the current
+  // cooldown for this item and we can leave.
+  if (cmd.words.length === 2) {
+    const curCool = (item.cooldown === 0) ? 'no cool down' :
+                     `a cool down of ${cooldownToString(item.cooldown)}`;
+
+    api.chat.say(`the cool down timer for ${item.name} is currently set to ${curCool}`);
+    return;
+  }
+
+  // Parse the given cooldown time specification.
+  const cooldown = stringToCooldown(api, intervalArg);
+  if (cooldown === undefined) {
+    api.chat.say(`${intervalArg} is not a valid cool down specification; valid formats are: ${specs.join(',')}`);
+    return;
+  }
+
+  const newCool = (cooldown === 0) ? 'no cool down' :
+                   `a cool down of ${cooldownToString(cooldown)}`;
+
+  // Update the responder with the changes, then report them.
+  await persistItemChanges(api, 'responders', item, { cooldown });
+  api.chat.say(`the cool down time for ${item.name} has been set to ${newCool}`);
 }
 
 
@@ -82,32 +264,32 @@ function modify_text_cooldown(api, cmd, userInfo) {
  * operations, which it will dispatch out to the other handlers above. Some of
  * these mimic similar options that are used by the command code to handle
  * similar operations. */
-function generate_text_response(api, cmd, userInfo) {
+async function generate_text_response(api, cmd, userInfo) {
   // If we got any arguments at all, dispatch based on what the selected
   // operation is. If none match, we fall through to the code that assumes no
   // arguments and describes overall usage.
   if (cmd.words.length !== 0) {
     switch (cmd.words[0]) {
       case 'add':
-        return add_text_response(api, cmd, userInfo);
+        return await add_text_response(api, cmd, userInfo);
 
       case 'remove':
-        return remove_text_response(api, cmd, userInfo);
+        return await remove_text_response(api, cmd, userInfo);
 
       case 'edit':
-        return edit_text_response(api, cmd, userInfo);
+        return await edit_text_response(api, cmd, userInfo);
 
       case 'link':
-        return modify_text_link(api, cmd, userInfo);
+        return await modify_text_link(api, cmd, userInfo);
 
       case 'unlink':
-        return modify_text_link(api, cmd, userInfo);
+        return await modify_text_link(api, cmd, userInfo);
 
       case 'access':
-        return modify_text_access(api, cmd, userInfo);
+        return await modify_text_access(api, cmd, userInfo);
 
       case 'cooldown':
-        return modify_text_cooldown(api, cmd, userInfo);
+        return await modify_text_cooldown(api, cmd, userInfo);
     }
   }
 
