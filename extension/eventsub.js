dc2720e52f19c236715a5fec2210118ecce5d2e1
Terence Martin <github@nurdz.com>
2021-12-29 13:26:57 -0800

    Use the events from the new list to setup events
    Similar to the previous change, this updates the code that is used to
    set up event listeners so that instead of having its own redundant list
    of events, the setup happens based on the information available in the
    global event list.

--- extension/eventsub.js
+++ extension/eventsub.js
@@ -3,149 +3,9 @@
 
 const { getUserInfo } = require('./auth');
 const { dedent: _ } = require('./utils');
+const { twitch_event_list } = require('./event_list');
 
 const { ReverseProxyAdapter, ConnectionAdapter, EventSubListener } = require('@twurple/eventsub');
-
-
-// =============================================================================
-
-
-/* This associates a list of events that we know how to handle with the EventSub
- * endpoint that knows how to handle them. In this mapping, the key is the
- * event name given in the events table and the value is the name of a method
- * in the Twurple EventSubListener class that sets up a listener for that event.
- *
- * Each item is annotated with a comment that contains an example Twitch CLI
- * invocation that allows you to simulate that event.
- *
- * To use that, install the Twitch CLI and make sure that there are environment
- * variables TWITCHBOT_SIGNING_SECRET, TWITCH_USERID and TWITCH_URI are set,
- * which specify the signing secret you're using for the bot, and the userID of
- * your channel and the reverse proxy URI you're using respectively. */
-const eventHandlers = {
-  //----------------------------//
-  // No special scopes required //
-  //----------------------------//
-
-  // twitch event trigger follow -s channel.follow.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.follow.$TWITCH_USERID
-  follow: 'subscribeToChannelFollowEvents',
-
-  // twitch event trigger streamup -s stream.online.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/stream.online.$TWITCH_USERID
-  streamOnline: 'subscribeToStreamOnlineEvents',
-
-  // twitch event trigger streamdown -s stream.offline.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/stream.offline.$TWITCH_USERID
-  streamOffline: 'subscribeToStreamOfflineEvents',
-
-  // twitch event trigger stream-change -s channel.update.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.update.$TWITCH_USERID
-  update: 'subscribeToChannelUpdateEvents',
-
-  // twitch event trigger raid -s channel.raid.to.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.raid.to.$TWITCH_USERID
-  raidIn: 'subscribeToChannelRaidEventsTo',
-
-  // twitch event trigger raid -s channel.raid.from.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.raid.from.$TWITCH_USERID
-  raidOut: 'subscribeToChannelRaidEventsFrom',
-
-  //------------------//
-  // Scope: bits:read //
-  //------------------//
-
-  // twitch event trigger cheer -s channel.cheer.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.cheer.$TWITCH_USERID
-  cheer: 'subscribeToChannelCheerEvents',
-
-  //-------------------------//
-  // Scope: channel:moderate //
-  //-------------------------//
-
-  // twitch event trigger ban -s channel.ban.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.ban.$TWITCH_USERID
-  ban: 'subscribeToChannelBanEvents',
-
-  // twitch event trigger unban -s channel.unban.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.unban.$TWITCH_USERID
-  unban: 'subscribeToChannelUnbanEvents', // scope: moderation:read
-
-  //------------------------//
-  // Scope: moderation:read //
-  //------------------------//
-
-  // twitch event trigger add-moderator -s channel.moderator.add.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.moderator.add.$TWITCH_USERID
-  moderator_add: 'subscribeToChannelModeratorAddEvents',
-
-  // twitch event trigger remove-moderator -s channel.moderator.remove.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.moderator.remove.$TWITCH_USERID
-  moderator_remove: 'subscribeToChannelModeratorRemoveEvents',
-
-  //-----------------------------------//
-  // Scope: channel:read:subscriptions //
-  //-----------------------------------//
-
-  // twitch event trigger subscribe -s channel.subscribe.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.subscribe.$TWITCH_USERID
-  subscribe: 'subscribeToChannelSubscriptionEvents',
-
-  // twitch event trigger unsubscribe -s channel.subscription.end.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.subscription.end.$TWITCH_USERID
-  unsubscribe: 'subscribeToChannelSubscriptionEndEvents',
-
-  // twitch event trigger gift -s channel.subscription.gift.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.subscription.gift.$TWITCH_USERID
-  gift: 'subscribeToChannelSubscriptionGiftEvents',
-
-  // twitch event trigger subscribe-message -s channel.subscription.message.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.subscription.message.$TWITCH_USERID
-  subcribe_message: 'subscribeToChannelSubscriptionMessageEvents',
-
-  //--------------------------------//
-  // Scope: channel:read:hype_train //
-  //--------------------------------//
-
-  // twitch event trigger hype-train-begin -s channel.hype_train.begin.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.hype_train.begin.$TWITCH_USERID
-  hypeBegin: 'subscribeToChannelHypeTrainBeginEvents',
-
-  // twitch event trigger hype-train-progress -s channel.hype_train.progress.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.hype_train.progress.$TWITCH_USERID
-  hypeUpdate: 'subscribeToChannelHypeTrainProgressEvents',
-
-  // twitch event trigger hype-train-end -s channel.hype_train.end.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.hype_train.end.$TWITCH_USERID
-  hypeEnd: 'subscribeToChannelHypeTrainEndEvents',
-
-  //-----------------------------------------------------------------//
-  // Scope: channel:read:redemptions (or channel:manage:redemptions) //
-  //-----------------------------------------------------------------//
-
-  // twitch event trigger add-reward -s channel.channel_points_custom_reward.add.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.channel_points_custom_reward.add.$TWITCH_USERID
-  channelpoint_add: 'subscribeToChannelRewardAddEvents',
-
-  // twitch event trigger update-reward -s channel.channel_points_custom_reward.remove.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.channel_points_custom_reward.remove.$TWITCH_USERID
-  channelpoint_remove: 'subscribeToChannelRewardRemoveEvents',
-
-  // twitch event trigger remove-reward -s channel.channel_points_custom_reward.update.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.channel_points_custom_reward.update.$TWITCH_USERID
-  channelpoint_update: 'subscribeToChannelRewardUpdateEvents',
-
-  // twitch event trigger add-redemption -s channel.channel_points_custom_reward_redemption.add.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.channel_points_custom_reward_redemption.add.$TWITCH_USERID
-  channelpoint_redeem: 'subscribeToChannelRedemptionAddEvents',
-
-  //-----------------------------------------------------//
-  // Scope: channel:read:polls (or channel:manage:polls) //
-  //-----------------------------------------------------//
-
-  // twitch event trigger poll-begin -s channel.poll.begin.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.poll.begin.$TWITCH_USERID
-  pollBegin: 'subscribeToChannelPollBeginEvents',
-
-  // twitch event trigger poll-progress -s channel.poll.progress.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.poll.progress.$TWITCH_USERID
-  pollUpdate: 'subscribeToChannelPollProgressEvents',
-
-  // twitch event trigger poll-end -s channel.poll.end.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.poll.end.$TWITCH_USERID
-  pollEnd: 'subscribeToChannelPollEndEvents',
-
-  //-----------------------------------------------------------------//
-  // Scope: channel:read:predictions (or channel:manage:predictions) //
-  //-----------------------------------------------------------------//
-
-  // twitch event trigger prediction-begin -s channel.prediction.begin.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.prediction.begin.$TWITCH_USERID
-  predictionBegin: 'subscribeToChannelPredictionBeginEvents',
-
-  // twitch event trigger prediction-progress -s channel.prediction.progress.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.prediction.progress.$TWITCH_USERID
-  predictionUpdate: 'subscribeToChannelPredictionProgressEvents',
-
-  // twitch event trigger prediction-lock -s channel.prediction.lock.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.prediction.lock.$TWITCH_USERID
-  predictionLock: 'subscribeToChannelPredictionLockEvents',
-
-  // twitch event trigger prediction-end -s channel.prediction.end.$TWITCH_USERID.$TWITCHBOT_SIGNING_SECRET -F https://$TWITCH_URI/event/channel.prediction.end.$TWITCH_USERID
-  predictionEnd: 'subscribeToChannelPredictionEndEvents',
-};
 
 
 // =============================================================================
@@ -193,36 +53,38 @@ async function handleAuthEvent(api, info) {
   api.log.info(`Setting up for incoming Twitch events`);
   await api.eventSub.listener.listen();
 
-  // The eventHandlers object maps our internal name for an event with the
-  // method in the EventSubListener class that sets it up.
+  // The twitch_event_list contains a list of all of the Twitch EventSub events
+  // that we want to handle, with each entry indicating our internal name for
+  // each event and the method in the EventSubListener class that is used to set
+  // up a listener for that event.
   //
   // To set up all of the listeners, we loop over all of the items and call the
-  // method to set things up.
+  // appropriate method to set things up.
   //
-  // Every handler has an identical method, which responds to the event by
+  // The handler for every event is identical and responds to the event by
   // looking up the event name in our internal event handler list and invoking
   // the defined function if it's found.
   //
   // Since each setup requires a potentially costly series of web requests to
   // finalize, we let them all run simultaneously and wait for them all to
   // finish.
-  Promise.all(Object.entries(eventHandlers).map(async ([name, handler]) => {
-    api.log.info(`Listening for event: ${name}`);
+  Promise.all(twitch_event_list.map(async ({internalName, setupHandler}) => {
+    api.log.info(`Listening for event: ${internalName}`);
     try {
       // The EventSubListener object contains a series of methods that allow you
       // to listen for specific incoming events. Look that up by indexing the
       // object directly.
-      await api.eventSub.listener[handler](info.userId, event => {
-        api.log.info(`received event: ${name}`);
-        const eventHandler = api.events.find(name);
+      await api.eventSub.listener[setupHandler](info.userId, event => {
+        api.log.info(`received event: ${internalName}`);
+        const eventHandler = api.events.find(internalName);
         if (eventHandler !== null) {
-          eventHandler.execute(api, name, event);
+          eventHandler.execute(api, internalName, event);
         } else {
-          api.log.error(`unable to find handler for event: ${name}`);
+          api.log.error(`unable to find handler for event: ${internalName}`);
         }
       });
     } catch (e) {
-      api.log.error(`Unable to set up handler for event ${name}: ${e}`);
+      api.log.error(`Unable to set up handler for event ${internalName}: ${e}`);
     }
   }));
 }
