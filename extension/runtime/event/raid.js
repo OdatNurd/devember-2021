988f0fe78689cec8e659d52d3ba5b62d3a66de25
Terence Martin <github@nurdz.com>
2021-12-29 22:33:29 -0800

    Set up an example event that auto-shouts out raids
    This includes a simple example of how one could handle incoming events
    by reacting to an incoming raid by shouting out the broadcaster who has
    raided in.
    
    This is done by "faking" up a command message and then asking the
    command to execute it as if the broadcaster manually typed it.
    
    This is commented out but could be easily enabled and extended, such as
    to make it delay to ensure that it doesn't conflict with any animated
    raid animations for example (which would also be something that you
    could manually trigger from here as well).

--- extension/runtime/event/raid.js
+++ extension/runtime/event/raid.js
@@ -13,6 +13,28 @@ const { displayEventDetails } = require('../../event_list');
 function incoming_raid(api, name, event) {
   // Display the properties of the event, for debug purposes.
   displayEventDetails(api, name, event);
+
+  // As an example of something that could be done, in response to an incoming
+  // raid, look up the shoutout command and, if found, execute it as if the
+  // broadcaster manually invoked it.
+  //
+  // This could also be done after a short delay to take advantage of not having
+  // the shout out conflict with any raid notifications or ads that the raiders
+  // may be stuck behind.
+
+  // // Check to see if there's a shoutout command, and if so, auto shout-out
+  // // people when they raid.
+  // const cmd = api.commands.find('$so');
+  // if (cmd !== null) {
+  //   // Parse a fake message that looks like someone is trying to should out the
+  //   // broadcaster that just raided, and then invoke the command.
+  //   //
+  //   // Here we use a fake user that has just enough fields for the command to
+  //   // treat the access level as broadcaster, who always has access to all
+  //   // commands that are enabled.
+  //   const details = api.cmdParser.parse(`${cmd.name} @${event.raidingBroadcasterName}`);
+  //   cmd.execute(api, details, {isBroadcaster: true});
+  // }
 }
 
 
